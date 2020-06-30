ca89f28a3f87315077709989335c3f435a500f9e
Yurek84 <ilnytskyj84@gmail.com>
2020-06-30 18:12:06 +0300

    Ínit

---
+++ assets/js/scripts.js
@@ -0,0 +1,62 @@
+//Задача 0
+var val = prompt();
+console.log(val);
+var a = typeof val === 'number' ? 'Не число' : 'Число';
+alert(a);
+
+// Задача 1
+// var a = prompt();
+// if (a >= 0, a <= 5) {
+//     alert('Верно');
+// }
+// else {
+//     alert('Неверно');
+// }
+
+//Задача 2
+// var num = prompt ();
+//  switch (num) {
+//      case '1' :
+//          alert ('Зима');
+//          break;
+//     case '2' :
+//         alert ('Весна');
+//         break;
+//     case '3' :
+//         alert ('Літо');
+//         break;
+//     case '4' :
+//         alert ('Осінь');
+//         break;
+//         default :
+//         alert ('Ви ввели неправильне значення');
+//         break;
+//  }
+
+//Задача 3
+// var a = prompt ('Введіть перше число');
+// var x = prompt ('Введіть операцію');
+// var b = prompt ('Введіть друге число');
+// // var a = Number;
+// // var b = Number;
+// switch (x) {
+//     case '+':
+//           result = a+b;
+//           alert( result );
+//         break;
+//     case '-':
+//         result = a-b;
+//         alert( result );
+//         break;
+//     case '*':
+//         result = a*b;
+//         alert( result );
+//         break;
+//     case '/':
+//         result = a/b;
+//         alert( result );
+//         break;
+//     default:
+//         alert ('Операція невідома. Повторіть ввід');
+//         break;
+// }
